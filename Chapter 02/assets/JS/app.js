f878e2a3818a46ea1bc06d2d47881a7e184831cd
aliza-ayaz16 <syedaalizaayaz.1276@gmail.com>
2023-04-09 05:15:20 +0500

    Chapter 2 completed

---
+++ Chapter 02/assets/JS/app.js
@@ -0,0 +1,56 @@
+//==========================Q1=====================================
+// var userName = "Aliza";
+//Check the output in console
+
+//==========================Q2=====================================
+// var myName = "Syeda Aliza Ayaz";
+//Check the output in console
+
+//==========================Q3=====================================
+// var message = "Hello World";
+// alert(message);
+
+//==========================Q4=====================================
+// let name = prompt("Enter Your Name");
+// alert(name);
+// let age = +prompt("Enter your age");
+// alert(age);
+// let qualified = prompt("Certified or Not??");
+// alert(qualified + " Mobile Application Development");
+
+//==========================Q5=====================================
+// let food = "PIZZA";
+// // console.log(food);
+
+// let final = food.slice(0, food.length-1);
+// // console.log(final);
+
+// final = food.slice(0, food.length-2);
+// // console.log(final);
+
+// final = food.slice(0, food.length-3);
+// // console.log(final);
+
+// final = food.slice(0, food.length-4);
+// // console.log(final);
+
+// final = food.slice(0, food.length-5);
+// // console.log(final);
+
+// alert();
+
+//==========================Q6=====================================
+// let email = "syedaalizaayaz.1276@gmail.com";
+// alert(email);
+
+//==========================Q7=====================================
+// let book = "A Smarter Way to Learn JavaScript.";
+// alert("I am trying to learn from the Book " + book); 
+
+//==========================Q8=====================================
+// let js = "Yah! I can write HTML content through JavaScript";
+// document.write(js);
+
+//==========================Q9=====================================
+// let symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(symbol)

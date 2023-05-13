35ada6ccdef8464fba8cc180b44c1a4d5e6ba431
Syeda Aliza Ayaz <syedaalizaayaz.1276@gmail.com>
2023-05-13 16:59:51 +0500

    Chapter 6 completed

---
+++ Chapter 05/assets/JS/app.js
@@ -0,0 +1,170 @@
+// ========================================== Question 01 ========================================== 
+// let no1 = +prompt("Enter first number:");
+// let no2 = +prompt("Enter second number:");
+// let sum = no1 + no2
+
+// document.write("The sum of " + no1 + " and " + no2 + " is " + sum);
+
+// ========================================== Question 02 ========================================== 
+
+// // // CAUTION: refresh 2 3 times after uncommenting the code to see the result
+
+// // Subtraction
+// let no1 = +prompt("Enter first number:");
+// let no2 = +prompt("Enter second number:");
+// let total = no1 - no2
+
+// document.write("The result of " + no1 + " - " + no2 + " is " + total);
+
+// // Multiplication
+// let no1 = +prompt("Enter first number:");
+// let no2 = +prompt("Enter second number:");
+// let result = no1 * no2
+
+// document.write("The result of " + no1 + " * " + no2 + " is " + result);
+
+// // Division
+// let no1 = +prompt("Enter first number:");
+// let no2 = +prompt("Enter second number:");
+// let result = no1 / no2
+
+// document.write("The result of " + no1 + " / " + no2 + " is " + result);
+
+// // Modulus
+// let no1 = +prompt("Enter first number:");
+// let no2 = +prompt("Enter second number:");
+// let result = no1 % no2
+
+// document.write("The remainder of " + no1 + " % " + no2 + " is " + result);  // // make sure to enter larger number in first prompt
+
+
+// ========================================== Question 03 ========================================== 
+// let number;
+
+// document.write("Value after variable declaration is: " + number + "<br>");
+
+// number = +prompt("Enter a number:");
+
+// document.write("Initial value: " + number + "<br>");
+
+// number++;
+
+// document.write("Value after increment is: " + number + "<br>");
+
+// number +=  7;
+
+// document.write("Value after addition is: " + number + "<br>");
+
+// number--;
+
+// document.write("Value after decrement is: " + number + "<br>");
+
+// number %= 3;
+
+// document.write("The remainder is: " + number);
+
+// ========================================== Question 04 ========================================== 
+// let costOfTickets = 600;
+
+// let noOfUserTicket = +prompt("How many tickets you want to buy???");
+
+// let totalTicketCost = noOfUserTicket * costOfTickets;
+
+// document.write("Total cost to buy " + noOfUserTicket + " tickets to a movie is " + totalTicketCost + " PKR");
+
+// ========================================== Question 05 ========================================== 
+// const table = +prompt("Enter the number of which you wan the multiplication table:");
+
+// document.write("Table of " + table + "<br>");
+
+// for (let i=0; i<=20; i++){
+//     document.write(table + " x " + i + " = " + table*i +"<br>");
+// }
+
+// ========================================== Question 06 ========================================== 
+// let todayTempinCelsius  = +prompt("Enter a temperature in Celsius: ");
+// const toFahrenheit = (todayTempinCelsius * 9/5) + 32 
+
+// let todayTempinFahrenheit = +prompt("Enter a temperature in Fahreheit: ");
+// const toCelsius = (todayTempinFahrenheit- 32) * 5/9  
+
+// document.write(todayTempinCelsius + "°C  is " + toFahrenheit + "°F" + "<br>" + todayTempinFahrenheit + "°F is " + toCelsius + "°C");
+
+// ========================================== Question 07 ========================================== 
+// let heading = "Shopping Cart";
+// document.write(heading + "<br>" + "<br>");
+
+// let price1 = +prompt("Enter Price of item 1:");
+// let quantity1 = +prompt("Quantity of item 1:");
+
+// let price2 = +prompt("Enter price of item 2:");
+// let quantity2 = +prompt("Quantity of item 2");
+
+// let charges = 100;
+
+
+// let totalCost = (price1 * quantity1) + (price2 * quantity2) + charges;
+
+// document.write("Price of item 1 is " + price1 + " Rs <br>" + "Quantity of Item 1 is " + quantity1 + "<br>" + "<br>" + "Price of item 2 is " + price2 + " Rs <br>" + "Quantity of item 2 is " + quantity2 + "<br>" + "Shipping Charges " + charges + "<br>" + "<br>" + "<br>" + "Total cost of your order is " + totalCost)
+
+
+// ========================================== Question 08 ==========================================
+// let heading = "Mark Sheet";
+// document.write(heading + "<br>" + "<br>" + "<br>" + "<br>")
+
+// let totalMarks = +prompt("Enter Total Marks:");
+// let marksObtained = +prompt("Enter Marks Obtained:");
+// let percentage = (marksObtained/totalMarks) * 100;
+
+// document.write("Total Marks: " + totalMarks + "<br>" + "Marks Obtained: " + marksObtained + "<br>" + "Percentage: " + percentage + "%")
+
+// ========================================== Question 09 ==========================================
+// let heading = "Currency in PKR"
+// document.write(heading + "<br>" + "<br>")
+
+// let currencyInUS$ = 10;
+// let currencyInRiyal = 25;
+
+// let currencyInPKR = (currencyInUS$ * 104.80) + (currencyInRiyal * 28);
+
+// document.write("Total Currency in PKR: " + currencyInPKR)
+
+// ========================================== Question 10 ==========================================
+// let num = +prompt("Enter a number:") + 5 * 10 / 2;
+
+// document.write(num);
+
+// ========================================== Question 11 ==========================================
+// let heading = "Age Calculator"
+// document.write(heading + "<br> <br>")
+
+// let currentYear = 2023;
+// let birthYear = +prompt("Enter your birth year:");
+
+// let yourAge = currentYear - birthYear;
+
+// document.write("Current Year: " + currentYear + "<br>" + "Birth Year: " + birthYear + "<br>" + "Your Age is: " + yourAge )
+
+// ========================================== Question 12 ==========================================
+// let heading = "The Geometrizer";
+// document.write(heading + "<br> <br>")
+
+// let radius = +prompt("Enter a value: ");
+// let circum = 2 * 3.142 * radius;
+// let area = 3.142 * radius ** 2
+
+// document.write("Radius of a circle is: " + radius + "<br>" + "The circumference is: " + circum + "<br>" + "The area is: " + area) 
+
+// ========================================== Question 13 ========================================== 
+// let heading = "The Lifetime Supply Calculator";
+// document.write(heading + "<br>" + "<br>");
+
+// let myFavSnack = prompt("Your Favourite snack:");
+// let myCurrentAge = +prompt("Your current age:");
+// let estimatedMaxAge = +prompt("Estimated Maximum age: ");
+// let amountPerDay = +prompt("How much would you consume it per day?");
+// let total = (estimatedMaxAge - myCurrentAge) * amountPerDay;
+
+// document.write("Favourite Snack: " + myFavSnack + "<br>" + "Current age: " + myCurrentAge + "<br>" + "Estimated Maximum Age: " + estimatedMaxAge + "<br>" + " Amount of snacks per day: " + amountPerDay + "<br>" + "You will need " + total + " " + myFavSnack + " to last you until the ripe old age of " + estimatedMaxAge);
+
+// ========================================== END ========================================== 

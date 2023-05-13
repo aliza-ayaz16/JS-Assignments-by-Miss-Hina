f005ae0eac1783fa6e1a2cc0b7ca0411f8a18cd8
Syeda Aliza Ayaz <syedaalizaayaz.1276@gmail.com>
2023-05-13 12:46:35 +0500

    Chapter 03 completed

---
+++ Chapter 03/assets/JS/app.js
@@ -0,0 +1,34 @@
+//========================================== Question 01========================================== 
+// let myAge = 16;
+
+// alert("I am " + myAge + " years old");
+
+// ========================================== Question 02 ========================================== 
+// let numberOfVisits = 0
+
+// let numberOfVisit = localStorage.getItem("numberOfVisits");
+
+// if (localStorage.getItem("numberOfVisits")) {
+//     localStorage.setItem("numberOfVisits", parseInt(localStorage.getItem("numberOfVisits")) + 1);
+//   } else {
+//     localStorage.setItem("numberOfVisits", 1);
+//   }
+  
+// alert("You have visited this site " + numberOfVisit + " times");
+
+// ========================================== Question 03 ========================================== 
+// let birthYear = 2006;
+
+// document.write("My birth year is " + birthYear +"<br>");
+// document.write("Data type of my declared variable is " + typeof(birthYear));
+
+// ========================================== Question 04 ========================================== 
+// let userName = prompt("Enter your name:");
+
+// let productTitle = prompt("What product do you want to buy?");
+
+// let quantity = +prompt("How many products do you want to order?");
+
+// document.write(userName + " ordered " + quantity + " " + productTitle + "(s) " + " on XYZ Clothing Store.")
+
+// ========================================== END ========================================== 

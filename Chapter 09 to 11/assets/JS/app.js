3f666d35ebcd92de1ff8e37438ed84909c4392cb
Syeda Aliza Ayaz <syedaalizaayaz.1276@gmail.com>
2023-05-13 20:53:21 +0500

    Q3 & Q11 remaining

---
+++ Chapter 09 to 11/assets/JS/app.js
@@ -0,0 +1,195 @@
+// ========================================== Question 01 ========================================== 
+// let userCity = prompt("Enter your city:");
+
+// if (userCity === "Karachi" || userCity === "karachi"){
+//     alert("Welcome to the city of lights");
+// }
+// else {
+//     alert("You are welcome");
+// }
+
+// ========================================== Question 02 ========================================== 
+// let gender = prompt("Enter your Gender:", "Male or Female");
+
+// if (gender === "Male" || gender === "male"){
+//     alert("Good Morning Sir");
+// }
+
+// else if (gender === "Female" || gender === "female"){
+//     alert("Good Morning Ma'am")
+// }
+
+// else {
+//     alert("Good Morning")
+// }
+
+// ========================================== Question 03 ========================================== 
+
+//smjh nhi aa rha tha, will complete after asking ma'am
+
+// ========================================== Question 04 ========================================== 
+// let remainingFuel = +prompt("How much fuel is left in your car");
+
+// if(remainingFuel <= 0.25){
+//     alert("Please refill the fuel in your car");
+// }
+
+// else if (remainingFuel < 0){
+//     alert("Make sure you have entered a possible value");
+// }
+
+// else {
+//     alert("You are going great")
+// }
+
+// ========================================== Question 05 ========================================== 
+// //a. 
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// //it worked
+
+// //b. 
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// // condition would be true if either b=83 or ++b is written
+
+// //c.
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// // condition 2 and 4 are true
+
+// //d. 
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// //it worked
+
+// //e. 
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// // it worked
+
+// //f. 
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// // it worked
+
+// ========================================== Question 06 ========================================== 
+// let userInput1 = +prompt("Enter your marks in first subject");
+// let userInput2 = +prompt("Enter your marks in second subject");
+// let userInput3 = +prompt("Enter your marks in third subject");
+
+// let totalMarksObt = userInput1 + userInput2 + userInput3;
+// let totalMarks = +prompt("What were the total marks?");
+
+// let heading = "Mark Sheet";
+// document.write(heading + "<br>" + "<br>" + "<br>" + "<br>")
+
+// let percentage = (totalMarksObt/totalMarks) * 100;
+
+// document.write("Total Marks: " + totalMarks + "<br>" + "Marks Obtained: " + totalMarksObt + "<br>" + "Percentage: " + percentage + "% <br>")
+// if (totalMarksObt >= 80){
+//     document.write("Grade: A-one <br>");
+//     document.write("Remarks: Excellent");
+// }
+
+// else if(totalMarksObt >= 70){
+//     document.write("Grade: A <br>");
+//     document.write("Remarks: Good");
+// }
+// else if(totalMarksObt >= 60){
+//     document.write("Grade: B <br>");
+//     document.write("Remarks: You need to improve");
+// }
+// else if(totalMarksObt < 60){
+//     document.write("Grade: Fail <br>");
+//     document.write("Remarks: Sorry");
+// }
+
+// ========================================== Question 07 ========================================== 
+// let secretNum = 9;
+
+// let userInp = +prompt("Guess a number from 1 to 10")
+
+// if(userInp === secretNum){
+//     alert("Bingo");
+// }
+// else if (userInp+1 === secretNum){
+//     alert("You are close enough");
+// }
+
+// else {
+//     alert("Better luck next time!");
+// }
+
+// ========================================== Question 08 ========================================== 
+// let userNum = +prompt("Enter a number:");
+
+// if(userNum%3 === 0){
+//     alert("The number is divisible by 3")
+// }
+// else if(userNum%3 !== 0) {
+//     alert("The number is not divisible by 3");
+// }
+
+// ========================================== Question 09 ========================================== 
+// let userInpNum = +prompt("Enter a number:");
+
+// if (userInpNum % 2 === 0) {
+//     alert(userInpNum + " is even")
+// }
+
+// else {
+//     alert(userInpNum + " is odd")
+// }
+
+// ========================================== Question 10 ==========================================
+// let userTemp = +prompt("Enter today's temperature:");
+
+// if(userTemp>=40){
+//     alert("It is too hot outside");
+// }
+// else if (userTemp>=30){
+//     alert("The weather today is normal");
+// }
+// else if(userTemp>=20){
+//     alert("Today's weather is cool");
+// }
+
+// else if(userTemp>=10){
+//     alert("OMG! Today's weather is so cool")
+// }
+
+// else if (userTemp<10){
+//     alert("Try to stay at home!!!")
+// }
+
+// ========================================== Question 11 ==========================================
+
+
+// // smjh nhi aa rha
+
+// ========================================== END ========================================== 

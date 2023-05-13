2ce5326cf6db2b0d9c502f60f808aff0b225b05c
Syeda Aliza Ayaz <syedaalizaayaz.1276@gmail.com>
2023-05-13 20:51:04 +0500

    Chapter 12 & 13 completed

---
+++ Chapter 12 to 13/assets/JS/app.js
@@ -0,0 +1,98 @@
+// ========================================= Question 01 =========================================
+// let userInp = prompt("Enter a character:", "a or 1");
+
+// if (userInp >= '0' && userInp <= '9') {
+//   console.log('The character is a number.');
+// } else if (userInp >= 'A' && userInp <= 'Z') {
+//   console.log('The character is an uppercase letter.');
+// } else if (userInp >= 'a' && userInp <= 'z') {
+//   console.log('The character is a lowercase letter.');
+// } else {
+//   console.log('The character is not alphanumeric.');
+// }
+
+// ========================================= Question 02 =========================================
+// let userNum = +prompt("Enter first number:");
+// let userNum2 = +prompt("Enter second number:");
+
+// if (userNum > userNum2){
+//     console.log(userNum + " is greater");
+// }
+// else if( userNum2 > userNum){
+//     console.log(userNum2 + " is greater");
+// }
+// else if (userNum===userNum2){
+//     console.log("Both numbers are equal");
+// }
+// else {
+//     console.log("Invalid input")
+// }
+
+// ========================================= Question 03 =========================================
+// let userInput = +prompt("Enter either a positive number, a negative number or zero:")
+
+// if(userInput>0){
+//     console.log(userInput + " is a positive integer");
+// }
+// else if(userInput<0){
+//     console.log(userInput + " is a negative integer");
+// }
+// else{
+//     console.log(userInput + " is a zero")
+// }
+
+// ========================================= Question 04 =========================================
+// let userCharacter = prompt("Enter an alphabet:");
+
+// if(userCharacter==="a" || userCharacter==="e" || userCharacter==="i" || userCharacter==="o" || userCharacter==="u"){
+//     console.log(true);
+// }
+// else{
+//     console.log(false)
+// }
+
+// ========================================= Question 05 =========================================
+// const passWord = "Hi";
+
+// let userPassword = prompt("Enter your password:");
+
+// if (userPassword===""){
+//     alert("Please enter your password!");
+// }
+// else if (userPassword === passWord){
+//     alert("Correct! The password you entered matches the original password.")
+// }
+// else {
+//     alert("Incorrect password")
+// }
+
+// ========================================= Question 06 =========================================
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     greeting = "Good Day";
+// } // // this curly bracket was missing
+// else {
+//     greeting = "Good Evening";
+// }
+
+// ========================================= Question 07 =========================================
+// let userTime = +prompt("Enter timein 24 hour format without :", "like this, 1900");
+
+// if (userTime >= 0000 && userTime < 1200) {
+//     alert("Good Morning!");
+// }
+// else if (userTime >= 1200 && userTime < 1700) {
+//     alert("Good afternoon!");
+// }
+// else if (userTime >= 1700 && userTime < 2100) {
+//     alert("Good evening!");
+// }
+// else if (userTime >= 2100 && userTime <= 2359) {
+//     alert("Good night!");
+// }
+// else {
+//     alert("Invalid input!!!");
+// }
+
+// ========================================= END =========================================

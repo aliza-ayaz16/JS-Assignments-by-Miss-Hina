2580477eb723fb0e03c8eb8926d1db63ec5635d3
Syeda Aliza Ayaz <syedaalizaayaz.1276@gmail.com>
2023-05-13 13:13:14 +0500

    Chapter 04 completed

---
+++ Chapter 04/assets/JS/app.js
@@ -0,0 +1,35 @@
+// ========================================== Question 01 ========================================== 
+// let myName = "Syeda Aliza Ayaz", myAge = 16, myBirthYear = 2006;
+
+// ========================================== Question 02 ========================================== 
+// // Legal
+// let myName = "Syeda Aliza Ayaz";
+// let my_age = 16;
+// let my$city = "Karachi";
+// let myClass = 11;
+// let $call = 0312-1234567;
+
+// // Illegal
+// let 8ty = 80;
+// let user.name = "Aliza";
+// let user name = "Aliza";
+// let user#name = "Aliza";
+// let var = variable;
+
+// ========================================== Question 03 ========================================== 
+// let eg = "$my_1stVariable";
+// let eg1 = "$name";
+// let eg2 = "_name";
+// let eg3 = "name";
+
+// let heading = "Rules for naming JS variables:";
+// let stat1 = "1)" + " Variable names can only contain camelCase, underscore(_) sign, dollar($) sign and shall not contain spaces.";
+// let stat2 = "For example: " + eg;
+// let stat3 = "2)" + " Variable must begin with a $, _ or lowercase.";
+// let stat4 = "For example: " + eg1 + ", " + eg2 + ", or " + eg3;
+// let stat5 = "3)" + " Variable names are case sensitive.";
+// let stat6 = "4)" + " Variable names should not be JS reserved keywords."; 
+
+// document.write(heading + "<br>" + "<br>" + stat1 + "<br>" + stat2 + "<br>" + "<br>" + stat3 + "<br>" + stat4 + "<br>" + "<br>" + stat5 + "<br>" + "<br>" + stat6);
+
+// ========================================== END ========================================== 
